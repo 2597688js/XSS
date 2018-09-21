f6a977e47741db177b17ee4b4206754c79982300
Janarddan Sarkar <31390987+2597688js@users.noreply.github.com>
2018-09-22 02:04:15 +0530

    Add files via upload

---
+++ admindash.php
@@ -0,0 +1,27 @@
+
+<?DOCTYPE HTML>
+<html lang="en_US">
+<head>
+	<meta charset="UTF-8">
+	<title>Banking System</title>
+
+	<link rel="stylesheet" type="text/css" href="intern.css">
+</head>
+<body>
+
+	<h1 align="center">Vulnerabilities</h1>
+	<h4 align="right" style="margin-right:50px;"><a href="logout.php">Logout</a></h4>
+	<div class="dashboard">
+		<table id="showDetails" border="1" style="width:30%;" align="center">
+			<tr>
+				<td><a href="xss_s.php">Stored XSS</a></td>
+			</tr>
+			<tr>
+				<td><a href="csrf.php">CSRF</a></td>
+			</tr>
+			
+		</table>
+	</div>
+	
+</body>
+</html>
